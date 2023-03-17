ca3a06468cec0068dea2923f413ae602faee14b1
kshwi <kshwiss@gmail.com>
2023-03-16 21:26:32 -0500

    tweaking the fetch logic

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-var BaseURL = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=4f532c03f12a6d8603036fbfb4f2b105`;
-var city = "Beijing";
+var BaseURL = `https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=` + Key;
+var lat = ???;
+var lon = ???;
+var city = "Dallas";
 var Key = "4f532c03f12a6d8603036fbfb4f2b105";
 var TestURL =
   "api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + Key;
   var 2ndTestURL = "api.openweathermap.org/data/2.5/forecast?id=524901&appid=" + Key;
 function Fetcher(URL) {
-  fetch(2ndTestURL)
+  fetch(BaseURL)
     .then(function(response) {
       return response.json();
     })
