4c8f14db8f70254f996336dd38ffc0fc5968f70b
kshwi <kshwiss@gmail.com>
2023-03-27 23:44:33 -0500

    troubleshooting javascript

--- script.js
+++ script.js
@@ -24,31 +24,33 @@ function Fetcher() {
     .then(function(data) {
       console.log(data);
       for (let i = 0; i < 5; i++) {
-        console.log(data[i]);
+        console.log(data.list[i]);
         var dateToday = dayjs.unix(data.list[i].dt);
         date = document.querySelector("#date" + i);
         var IconPlace = document.querySelector("#Iconic" + i);
         var Temp = document.querySelector("#temp" + i);
         var Wind = document.querySelector("#wind" + i);
         var Humidity = document.querySelector("#humidity" + i);
+        var IconElement = document.createElement("img");
         var iconUrl =
           "http://openweathermap.org/img/wn/" +
           data.list[i].weather[0].icon +
           ".png";
 
-        IconPlace.innerHTML = iconUrl;
-        IconPlace.setAttribute("src", iconUrl);
+        // IconElement = iconUrl;
+        IconElement.setAttribute("src", iconUrl);
         date.textContent = dateToday.format("dddd MM/DD/YYYY");
         Temp.textContent = data.list[i].main.temp + "°F";
         Wind.textContent = data.list[i].wind.speed + "mph";
         Humidity.textContent = data.list[i].main.humidity + "°F";
+        IconPlace.appendChild(IconElement);
       }
       history1.push(" " + city);
       localStorage.setItem("history", history1);
       var SearchHistory = document.getElementById("searchHistory");
       SearchHistory.innerHTML = localStorage.getItem("history");
       // currentlyCity.value = city;
-      BigCity.value = localStorage.getItem("history");
+      BigCity.innerHTML = localStorage.getItem("history");
     });
 }
 
