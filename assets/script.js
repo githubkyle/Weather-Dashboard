089e8a23d0cf595b72be3b62696cc90a8f62b716
kshwi <kshwiss@gmail.com>
2023-04-16 16:16:19 -0500

    added colors and attempted to fix the cities so they would be a clickable event. I'll return to complete that part and I'm also working on preventing multiple weather icons being displayed

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,8 @@ var userContainer = document.getElementById("display");
 var fetchButton = document.getElementById("fetch-button");
 var BigCity = document.querySelector("#chosenCity");
 var city = document.getElementById("cityName").value;
+const Deletion = document.getElementsByClassName("pic");
+
 function Fetcher() {
   var city = document.getElementById("cityName").value;
   var Key = "4f532c03f12a6d8603036fbfb4f2b105";
@@ -25,33 +27,47 @@ function Fetcher() {
       console.log(data);
       for (let i = 0; i < 5; i++) {
         console.log(data.list[i]);
-        var dateToday = dayjs.unix(data.list[i].dt);
-        date = document.querySelector("#date" + i);
+
+        var date = new Date();
+        date.setDate(date.getDate() + i);
+        dateEl = document.querySelector("#date" + i);
         var IconPlace = document.querySelector("#Iconic" + i);
         var Temp = document.querySelector("#temp" + i);
         var Wind = document.querySelector("#wind" + i);
         var Humidity = document.querySelector("#humidity" + i);
         var IconElement = document.createElement("img");
+
         var iconUrl =
           "http://openweathermap.org/img/wn/" +
           data.list[i].weather[0].icon +
           ".png";
 
+        console.log(iconUrl);
+        dateEl.textContent = "";
+        Temp.textContent = "";
+        Wind.textContent = "";
+        Humidity.textContent = "";
+
         // IconElement = iconUrl;
         IconElement.setAttribute("src", iconUrl);
-        date.textContent = dateToday.format("dddd MM/DD/YYYY");
+        dateEl.textContent = date.toDateString();
         Temp.textContent = data.list[i].main.temp + "°F";
         Wind.textContent = data.list[i].wind.speed + "mph";
         Humidity.textContent = data.list[i].main.humidity + "°F";
+
+        new Date();
         IconPlace.appendChild(IconElement);
       }
+
       history1.push(" " + city);
       localStorage.setItem("history", history1);
       var SearchHistory = document.getElementById("searchHistory");
       SearchHistory.innerHTML = localStorage.getItem("history");
       // currentlyCity.value = city;
-      BigCity.innerHTML = localStorage.getItem("history");
-    });
+      BigCity.innerHTML = `${city} Weather Today:`;
+      currentlyCity.innerHTML = "";
+    })
+    .catch(err => (currentlyCity.innerHTML = "Unable to search that city"));
 }
 
 var Searcher = document.getElementById("searcher");
